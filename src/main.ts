ffb14433c9e8c78c6d54a3f807a8786677987d36
gw31415 <git@amas.dev>
2023-06-24 01:08:28 +0900

    format

--- src/main.ts
+++ src/main.ts
@@ -11,23 +11,22 @@ import { onHover } from "./handler/hover"
 import { onDefinition } from "./handler/definition"
 import { updateObsidianNotes } from "./common/vault"
 
-connection.onInitialize((_params: InitializeParams) => {
-	return {
-		capabilities: {
-			textDocumentSync: TextDocumentSyncKind.Incremental,
-			completionProvider: {
-				resolveProvider: true,
-			},
-			hoverProvider: true,
-			definitionProvider: true,
-			workspace: {
-				workspaceFolders: {
-					supported: true,
-				}
+connection.onInitialize((_params: InitializeParams) => ({
+	capabilities: {
+		textDocumentSync: TextDocumentSyncKind.Incremental,
+		completionProvider: {
+			resolveProvider: true,
+		},
+		hoverProvider: true,
+		definitionProvider: true,
+		workspace: {
+			workspaceFolders: {
+				supported: true,
 			},
 		},
-	}
-})
+	},
+}))
+
 connection.onInitialized(updateObsidianNotes)
 
 connection.onCompletion(onCompletion)
